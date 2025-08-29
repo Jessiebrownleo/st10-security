1b416595842ad8cb8dc44d4c8387c3ab93bc018a
jessiebrownleo <sobengg5@gmail.com>
2025-08-29 09:47:14 +0700

    feat(user) : User management

--- src/main/java/com/example/st10security/config/SecurityConfig.java
+++ src/main/java/com/example/st10security/config/SecurityConfig.java
@@ -43,6 +43,7 @@ public class SecurityConfig {
                         auth -> auth
                                 .requestMatchers("/","/home").permitAll()
                                 .requestMatchers("/admin").hasRole("ADMIN")
+                                .requestMatchers("/users").hasRole("ADMIN")
                                 .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
